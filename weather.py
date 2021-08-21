c1e2491c8de975460b8c99cb1853a62e5ca372d3
srishti011 <48403800+srishti011@users.noreply.github.com>
2021-08-21 12:52:21 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,62 +1,30 @@
-from tkinter import*
 import requests
+import os
+from datetime import datetime
 
-class weather:
-    def __init__(self):
-        self.root=Tk()
-       
-        self.root.title("Live Weather Forecast")
-        self.root.minsize(400,700)
-        self.root.maxsize(400,700)
+user_api = '2726e8a040275ec5d6e797603da76e34'
 
-        self.root.configure(background="#00a65a")
-       
-        self.label1=Label(self.root,text="Weather Forecast",bg="#00a65a",fg="#fff")
-        self.label1.configure(font=("Roman",22,"bold"))
-        self.label1.pack(pady=(30,10))
+location = input("Enter the city name: ")
 
-        self.City=Entry(self.root)
-        self.City.pack(ipadx=40,ipady=5)
-       
-        self.click=Button(self.root,text="Enter City",bg="#fff",fg="#000",width=23,height=2,command=lambda:self.__fetch())
-        self.click.configure(font=("Constantia",10,"bold"))
-        self.click.pack(pady=(10,20))
+complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+user_api
+api_link = requests.get(complete_api_link)
+api_data = api_link.json()
+# print(api_data)
 
-        self.result=Label(self.root,text="",bg="#00a65a",fg="#fff")
-        self.result.configure(font=("Constantia",12,"bold"))
-        self.result.pack(pady=(5,10))
+#create variables to store and display data
 
-       
-        self.root.mainloop()
+temp_city = ((api_data['main']['temp']) - 273.15)
+weather_desc = api_data['weather'][0]['description']
+hmdt = api_data['main']['humidity']
+wind_spd = api_data['wind']['speed']
+date_time = datetime.now().strftime("%d %b %Y | %I:%M:%S %p")
 
+print ("-------------------------------------------------------------")
+print ("Weather Stats for - {}  || {}".format(location.upper(), date_time))
+print ("-------------------------------------------------------------")
 
+print ("Current temperature is: {:.2f} deg C".format(temp_city))
+print ("Current weather desc  :",weather_desc)
+print ("Current Humidity      :",hmdt, '%')
+print ("Current wind speed    :",wind_spd ,'kmph')
 
-    def __fetch(self):
-        city=self.City.get()
-        url="http://api.openweathermap.org/data/2.5/weather?q={}&appid=8320daf3dd5b9489474dd07bb048bab3".format(self.City.get().upper())
-        try:
-            response=requests.get(url)
-            data=response.json()
-        #print(data["main"])
-        #print(type(data["main"]))
-            labeltext = ""
-            for i in list(data["main"].keys()):
-                labeltext += i + ": " + str(data["main"][i])+ "\n"
-            #self.label3=Label(self.root,text="{}".format(labeltext),bg="#00a65a",fg="#fff")
-            #self.label3.configure(text="{}".format(labeltext))
-            #self.label3.pack()
-        except:
-            labeltext = "WRONG CITY"
-       
-           
-        self.result.configure(text=labeltext)
-"""
-        //city=""
-        //for i in data['main']:
-          //  city= city + int(i['temp'])+" | " +i['humidity'] + " | " +i['pressure']
-
-        //self.result.configure(text=city)
-"""
-       
-           
-obj=weather()
